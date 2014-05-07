2181fc7638a62a65ff29fb01b7f41273669def61
Daniel Santillan <daniel.santillan@eox.at>
2014-05-07 12:16:14 +0200

    Remove unnecessary data added small script

---
+++ cloudsat/load_Cloudsat_2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd /var/www/ngeo/ngeo_browse_server_instance
+
+python manage.py ngeo_browse_layer --add /var/vmanip/data/cloudsat/Cloudsat_browselayer.xml
+
+tmpdir=`mktemp -d`
+
+find /var/vmanip/data/cloudsat/ -name 'cloudsat_2014012*' -exec unzip {} -d ${tmpdir} \;
+#find /var/vmanip/data/cloudsat/ -name 'cloudsat_20140124150000_37519.zip' -exec unzip {} -d ${tmpdir} \;
+
+find ${tmpdir} -name '*tif' -exec mv {} /var/www/store/ \;
+#find ${tmpdir} -name '*tif' -exec mv {} /var/www/ngeo/store/ \;
+
+find ${tmpdir} -name '*xml' -exec python manage.py ngeo_ingest {} -v3 \;
+
+rm -rf ${tmpdir}
