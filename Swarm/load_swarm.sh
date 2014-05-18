9e7bdd692c3ab896584f4a8f7f872d73381f1aa1
Daniel Santillan <daniel.santillan@eox.at>
2014-05-18 20:57:22 +0200

    Added more browse layer definitions for swarm data

--- Swarm/load_swarm.sh
+++ Swarm/load_swarm.sh
@@ -3,13 +3,16 @@
 cd /var/www/ngeo/ngeo_browse_server_instance/
 
 
-python manage.py ngeo_browse_layer --add /var/vmanip/data/Swarm/Swarm_browselayer.xml
+python manage.py ngeo_browse_layer --add /var/vmanip/data/Swarm/SW_OPER_MAGC_LR_1B_layer.xml
+python manage.py ngeo_browse_layer --add /var/vmanip/data/Swarm/SW_OPER_MAGC_LR_1B_layer.xml
+python manage.py ngeo_browse_layer --add /var/vmanip/data/Swarm/SW_OPER_MAGC_LR_1B_layer.xml
+
 
 tmpdir=`mktemp -d`
 
-find /var/vmanip/data/Swarm/ -name 'swarm-browses*' -exec tar -xzvf {} -C ${tmpdir} \;
+find /var/vmanip/data/Swarm/ -name 'browses.tar.gz' -exec tar -xzvf {} -C ${tmpdir} \;
 
-find ${tmpdir} -name '*tif' -exec mv {} /var/www/ngeo/store/ \;
+find ${tmpdir} -name '*tif' -exec mv {} /var/www/ngeo/store/output/ \;
 
 find ${tmpdir} -name '*xml' -exec python manage.py ngeo_ingest {} -v3 \;
 
