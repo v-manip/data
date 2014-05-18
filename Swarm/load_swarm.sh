c04c208d27ca2f2f2f39c4629ebc8575df922160
Daniel Santillan <daniel.santillan@eox.at>
2014-05-18 21:09:41 +0200

    Small corrections to script

--- Swarm/load_swarm.sh
+++ Swarm/load_swarm.sh
@@ -3,8 +3,8 @@
 cd /var/www/ngeo/ngeo_browse_server_instance/
 
 
-python manage.py ngeo_browse_layer --add /var/vmanip/data/Swarm/SW_OPER_MAGC_LR_1B_layer.xml
-python manage.py ngeo_browse_layer --add /var/vmanip/data/Swarm/SW_OPER_MAGC_LR_1B_layer.xml
+python manage.py ngeo_browse_layer --add /var/vmanip/data/Swarm/SW_OPER_MAGA_LR_1B_layer.xml
+python manage.py ngeo_browse_layer --add /var/vmanip/data/Swarm/SW_OPER_MAGB_LR_1B_layer.xml
 python manage.py ngeo_browse_layer --add /var/vmanip/data/Swarm/SW_OPER_MAGC_LR_1B_layer.xml
 
 
@@ -12,7 +12,7 @@ tmpdir=`mktemp -d`
 
 find /var/vmanip/data/Swarm/ -name 'browses.tar.gz' -exec tar -xzvf {} -C ${tmpdir} \;
 
-find ${tmpdir} -name '*tif' -exec mv {} /var/www/ngeo/store/output/ \;
+find ${tmpdir} -name '*tif' -exec mv {} /var/www/ngeo/store/browses/ \;
 
 find ${tmpdir} -name '*xml' -exec python manage.py ngeo_ingest {} -v3 \;
 
