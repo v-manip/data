85579332fb9ab8aa9aa873c7d7ea4ccc0857bb07
Daniel Santillan <daniel.santillan@eox.at>
2014-05-23 15:21:30 +0200

    Added new layer definitions for ALARO and AROME, added them also to the respective load script

--- ALARO/load_ALARO.sh
+++ ALARO/load_ALARO.sh
@@ -8,12 +8,21 @@ echo "Loaded ALARO Browse Layer Specific Humidity"
 python manage.py ngeo_browse_layer --add /var/vmanip/data/ALARO/ALARO_Temperature_isobaric.xml
 echo "Loaded ALARO Browse Layer Temperature Isobaric"
 
+python manage.py ngeo_browse_layer --add /var/vmanip/data/ALARO/ALARO_Temperature_surface.xml
+echo "Loaded ALARO Browse Layer Temperature Surface"
+
+python manage.py ngeo_browse_layer --add /var/vmanip/data/ALARO/ALARO_Surface_Pressure.xml
+echo "Loaded ALARO Browse Layer Surface Pressure"
+
 
 find /home/sistema/DATA/useCase1-2/model/ALARO -name "*Specific_hu*.tif" -exec cp {} /var/www/ngeo/store/ \;
-
 find /home/sistema/DATA/useCase1-2/model/ALARO -name "*Specific_hu*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
 
-
 find /home/sistema/DATA/useCase1-2/model/ALARO -name "*Temperature_isoba*.tif" -exec cp {} /var/www/ngeo/store/ \;
-
 find /home/sistema/DATA/useCase1-2/model/ALARO -name "*Temperature_isoba*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
+
+find /home/sistema/DATA/useCase1-2/model/ALARO -name "*Temperature_surface*.tif" -exec cp {} /var/www/ngeo/store/ \;
+find /home/sistema/DATA/useCase1-2/model/ALARO -name "*Temperature_surface*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
+
+find /home/sistema/DATA/useCase1-2/model/ALARO -name "*Surface_Pressure*.tif" -exec cp {} /var/www/ngeo/store/ \;
+find /home/sistema/DATA/useCase1-2/model/ALARO -name "*Surface_Pressure*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
--- AROME/load_AROME.sh
+++ AROME/load_AROME.sh
@@ -8,12 +8,21 @@ echo "Loaded AROME Browse Layer Specific Humidity"
 python manage.py ngeo_browse_layer --add /var/vmanip/data/AROME/AROME_Temperature_isobaric.xml
 echo "Loaded AROME Browse Layer Temperature Isobaric"
 
+python manage.py ngeo_browse_layer --add /var/vmanip/data/AROME/AROME_Temperature_surface.xml
+echo "Loaded AROME Browse Layer Temperature Surface"
+
+python manage.py ngeo_browse_layer --add /var/vmanip/data/AROME/AROME_Surface_Pressure.xml
+echo "Loaded AROME Browse Layer Surface Pressure"
+
 
 find /home/sistema/DATA/useCase1-2/model/AROME -name "*Specific_hu*.tif" -exec cp {} /var/www/ngeo/store/ \;
-
 find /home/sistema/DATA/useCase1-2/model/AROME -name "*Specific_hu*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
 
-
 find /home/sistema/DATA/useCase1-2/model/AROME -name "*Temperature_isoba*.tif" -exec cp {} /var/www/ngeo/store/ \;
-
 find /home/sistema/DATA/useCase1-2/model/AROME -name "*Temperature_isoba*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
+
+find /home/sistema/DATA/useCase1-2/model/AROME -name "*Temperature_surface*.tif" -exec cp {} /var/www/ngeo/store/ \;
+find /home/sistema/DATA/useCase1-2/model/AROME -name "*Temperature_surface*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
+
+find /home/sistema/DATA/useCase1-2/model/AROME -name "*Surface_Pressure*.tif" -exec cp {} /var/www/ngeo/store/ \;
+find /home/sistema/DATA/useCase1-2/model/AROME -name "*Surface_Pressure*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
