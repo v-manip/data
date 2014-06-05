2e70e235c3343c6d3989fa6b9afd57da8255fe8f
Daniel Santillan <daniel.santillan@eox.at>
2014-06-05 14:28:45 +0200

    Added script to create cache

---
+++ cloudsat/create_cache.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -e
+
+# Add your custom configuration below.
+
+# Add alias
+cat << EOF >> /root/.bashrc
+alias l="ls -lah"
+EOF
+cat << EOF >> /home/vagrant/.bashrc
+alias l="ls -lah"
+EOF
+
+# Create gltf folder, which is then exported via httpd
+mkdir -p /var/www/cache/gltf
+chown apache.apache /var/www/cache/gltf
+
+# Create Cloudsat sqlite.db
+cat << EOF > /tmp/bootstrap_cloudsat.txt
+create table if not exists tiles(
+                        tileset text,
+                        grid text,
+                        x integer,
+                        y integer,
+                        z integer,
+                        data text,
+                        dim text,
+                        ctime datetime,
+                        primary key(tileset,grid,x,y,z,dim)
+                    );
+
+EOF
+sqlite3 /var/www/cache/Cloudsat.sqlite < /tmp/bootstrap_cloudsat.txt
+chown apache.apache /var/www/cache/Cloudsat.sqlite
+rm /tmp/bootstrap_cloudsat.txt
