1555a86b91c3fd768b3c14c3a719e236cbfcc4c8
Daniel Santillan <daniel.santillan@eox.at>
2014-06-05 11:56:36 +0200

    Added script to load FSC pan european daily data

---
+++ Cryo/load_FSC.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+tmpdir=`mktemp -d`
+
+find /home/santilland/ -name 'cryoland.tar.gz' -exec tar -zxvf {} -C ${tmpdir} \;
+
+find ${tmpdir} -name "*.tif" | while read F ; do sudo python manage.py eoxs_dataset_register -r Greyscale -d $F -m ${F//.tif/.tif.xml} --series daily_FSC_PanEuropean_Optical ; done
+
+rm -rf ${tmpdir}
