12c833c0a817aa30f0d2031ccd589888e70eb83f
Daniel Santillan <daniel.santillan@eox.at>
2014-06-05 14:30:36 +0200

    Clenad up script

--- cloudsat/create_cache.sh
+++ cloudsat/create_cache.sh
@@ -1,14 +1,5 @@
 #!/bin/sh -e
 
-# Add your custom configuration below.
-
-# Add alias
-cat << EOF >> /root/.bashrc
-alias l="ls -lah"
-EOF
-cat << EOF >> /home/vagrant/.bashrc
-alias l="ls -lah"
-EOF
 
 # Create gltf folder, which is then exported via httpd
 mkdir -p /var/www/cache/gltf
