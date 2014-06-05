671927f59126e907146cd76f0475ed0afb8a9c82
Daniel Santillan <daniel.santillan@eox.at>
2014-06-05 11:58:51 +0200

    tar of fsc is not zipped removed z from tar command

--- Cryo/load_FSC.sh
+++ Cryo/load_FSC.sh
@@ -2,7 +2,7 @@
 
 tmpdir=`mktemp -d`
 
-find /home/santilland/ -name 'cryoland.tar.gz' -exec tar -zxvf {} -C ${tmpdir} \;
+find /home/santilland/ -name 'cryoland.tar.gz' -exec tar -xvf {} -C ${tmpdir} \;
 
 find ${tmpdir} -name "*.tif" | while read F ; do sudo python manage.py eoxs_dataset_register -r Greyscale -d $F -m ${F//.tif/.tif.xml} --series daily_FSC_PanEuropean_Optical ; done
 
