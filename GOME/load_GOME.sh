7e37d5e9b968ae57e4bc4af267aeafeebbcbbc47
Daniel Santillan <daniel.santillan@eox.at>
2014-06-06 17:38:11 +0200

    Added GOME-2 layer definition and load script

---
+++ GOME/load_GOME.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd /var/www/ngeo/ngeo_browse_server_instance
+
+python manage.py ngeo_browse_layer --add /var/vmanip/data/GOME/GOME_browselayer.xml
+echo "Loaded GOME browse layer"
+
+
+find /home/sistema/DATA/useCase5/NP_L2_GOME2 -name "*.tif" -exec cp {} /var/www/ngeo/store/ \;
+find /home/sistema/DATA/useCase5/NP_L2_GOME2 -name "*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
+
