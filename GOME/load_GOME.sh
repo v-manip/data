c248f202e600f6eff70c121ebaca620b0e21d464
Daniel Santillan <daniel.santillan@eox.at>
2014-06-13 16:41:40 +0200

    Removed other layers from the definition and just left it for GOME

--- GOME/load_GOME.sh
+++ GOME/load_GOME.sh
@@ -9,9 +9,4 @@ echo "Loaded GOME browse layer"
 find /home/sistema/DATA/useCase5/NP_L2_GOME2 -name "*.tif" -exec cp {} /var/www/ngeo/store/ \;
 find /home/sistema/DATA/useCase5/NP_L2_GOME2 -name "*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
 
-find /home/sistema/DATA/useCase5/NP_L3_MRG -name "*.tif" -exec cp {} /var/www/ngeo/store/ \;
-find /home/sistema/DATA/useCase5/NP_L3_MRG -name "*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
-
-find /home/sistema/DATA/useCase5/REAN_MODEL -name "*.tif" -exec cp {} /var/www/ngeo/store/ \;
-find /home/sistema/DATA/useCase5/REAN_MODEL -name "*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
 
