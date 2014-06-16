b22e8ad104db779114fcae93c6fc29b4803542a1
Daniel Santillan <daniel.santillan@eox.at>
2014-06-16 16:06:55 +0200

    updated gome load and definition

--- GOME/load_GOME.sh
+++ GOME/load_GOME.sh
@@ -6,7 +6,7 @@ python manage.py ngeo_browse_layer --add /var/vmanip/data/GOME/GOME_browselayer.
 echo "Loaded GOME browse layer"
 
 
-find /home/sistema/DATA/useCase5/NP_L2_GOME2 -name "*.tif" -exec cp {} /var/www/ngeo/store/ \;
-find /home/sistema/DATA/useCase5/NP_L2_GOME2 -name "*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
+find /home/sistema/DATA/useCase5/NP_L2_GOME2/20140513_1700 -name "*.tif" -exec cp {} /var/www/ngeo/store/ \;
+find /home/sistema/DATA/useCase5/NP_L2_GOME2/20140513_1700 -name "*.xml" -exec python manage.py ngeo_ingest {} -v3 \;
 
 
